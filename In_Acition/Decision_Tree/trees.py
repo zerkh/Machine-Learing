986b75d0b62eaa06e7ebd156674577e3f314eea7
Zerokh <mhzx20@163.com>
2014-11-18 21:36:48 +0800

    Nov 18 21:36

--- In_Acition/Decision_Tree/trees.py
+++ In_Acition/Decision_Tree/trees.py
@@ -1,5 +1,8 @@
+#coding:UTF-8
 from math import log
+import operator
 
+#计算香农熵
 def calcShannonEnt(dataset):
     numEntries = len(dataset)
     labelCount = {}
@@ -22,3 +25,65 @@ def createDataSet():
                [0, 1, "no"]]
     labels = ["no surfacing", "flippers"]
     return dataSet, labels
+
+#数据分割
+def splitDataSet(dataSet, axis, value):
+    retDataSet = []
+    for featVec in dataSet:
+        if featVec[axis] == value:
+            reducedFeatVec = featVec[:axis]
+            reducedFeatVec.extend(featVec[axis+1:])
+            retDataSet.append(reducedFeatVec)
+    return retDataSet
+
+#ID3算法
+def chooseBestFeatureToSplit(dataSet):
+    numFeatures = len(dataSet[0])-1
+    baseEntropy = calcShannonEnt(dataSet)
+    bestInfoGain = 0.0
+    bestFeature = -1
+    for i in range(numFeatures):
+        featList = [example[i] for example in dataSet]
+        uniqueVals = set(featList)
+        newEntroy = 0.0
+        for value in uniqueVals:
+            subDataSet = splitDataSet(dataSet, i, value)
+            prob = float(len(subDataSet))/len(dataSet)
+            newEntroy += prob*calcShannonEnt(subDataSet)
+        infoGain = baseEntropy-newEntroy  #信息增益是熵的减少！
+        if (infoGain > bestInfoGain):
+            bestInfoGain = infoGain
+            bestFeature = i
+
+    return bestFeature
+            
+#多数表决的方法决定如何定义该叶子结点
+def majorityCnt(classList):
+    classCount = {}
+    for vote in classList:
+        if vote not in classCount:
+            classCount[vote] = 0
+        classCount[vote] += 1
+    
+    sortedClassCount = sorted(classCount.iteritems(), key = operator.itemgetter[1], reverse = True)
+    return sortedClassCount[0][0]
+
+#构建决策树
+def createTree(dataSet, labels):
+    classList = [example[-1] for example in dataSet]
+    if classList.count(classList[0]) == len(classList):
+        return classList[0]
+    if len(dataSet[0]) == 1:
+        return majorityCnt(classList)
+
+    bestFeat = chooseBestFeatureToSplit(dataSet)
+    bestFeatLabel = labels[bestFeat]
+    myTree = {bestFeatLabel:{}}
+    del(labels[bestFeat])
+    featValues = [example[bestFeat] for example in dataSet]
+    uniqueVals = set(featValues)
+    for value in uniqueVals:
+        subLabels = labels[:]
+        myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet, bestFeat, value), subLabels)
+
+    return myTree
