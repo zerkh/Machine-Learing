fa73350106c8af60ff337a001f2f9b6a09bdca86
zerkh <mhzx20@163.com>
2014-11-19 10:39:07 +0800

    Nov 19 10:39

--- In_Acition/Decision_Tree/treePlotter.py
+++ In_Acition/Decision_Tree/treePlotter.py
@@ -1,4 +1,4 @@
-#coding:gbk
+#coding:utf-8
 import matplotlib.pyplot as plt
 
 decisionNode = dict(boxstyle = "sawtooth", fc = "0.8")
@@ -18,3 +18,38 @@ def createPlot():
     plotNode (U'decisionNode', (0.5, 0.1), (0.1, 0.5), decisionNode)
     plotNode(U'leafNode', (0.8, 0.1), (0.3, 0.8), leafNode)
     plt.show()
+
+def getNumLeafs(myTree):
+    numLeafs = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            numLeafs += getNumLeafs(secondDict[key])
+        else:
+            numLeafs += 1
+
+        return numLeafs
+
+def getTreeDepth(myTree):
+    maxDepth = 0
+    firstStr = myTree.keys()[0]
+    secondDict = myTree[firstStr]
+    
+    for key in secondDict.keys():
+        if type(secondDict[key]).__name__ == 'dict':
+            thisDepth = 1+getTreeDepth(secondDict[key])
+        else:
+            thisDepth = 1
+        if thisDepth > maxDepth:
+            maxDepth = thisDepth
+        
+    return maxDepth 
+
+def retrieveTree(i):
+    listOfTrees = [
+        {'no surfacing': {0: 'no', 1: {'flippers': {0: 'no', 1: 'yes'}}}},
+        {'no surfacing': {0: 'no', 1: {'flippers': {0: {'head': {0: 'no', 1: 'yes'}}, 1: 'no'}}}}
+    ]
+    return listOfTrees[i]
